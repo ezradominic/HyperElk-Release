d0e11390a31d854bbf714e4ebcc8943e6d79a88e
mufflon11 <mufflon12@googlemail.com>
2020-11-23 18:17:32 +0100

    Brewmaster fixed Invoke Niuzao & Added Stagger Management + UseInvokeNiuzao option

--- Rotations/Monk/Brewmaster Monk.cs
+++ Rotations/Monk/Brewmaster Monk.cs
@@ -25,7 +25,10 @@ namespace HyperElk.Core
         private int CelestialBrewLifePercentProc => numbList[CombatRoutine.GetPropertyInt(CelestialBrew)];
         private int FortifyingBrewLifePercentProc => numbList[CombatRoutine.GetPropertyInt(FortifyingBrew)];
         private int HealingElixirLifePercentProc => numbList[CombatRoutine.GetPropertyInt(HealingElixir)];
-
+        string[] InvokeNiuzaoList = new string[] { "always", "with Cooldowns", "On AOE" };
+        string[] StaggerList = new string[] { "always", "Light Stagger", "Moderate Stagger", "Heavy Stagger" };
+        private string UseInvokeNiuzao => InvokeNiuzaoList[CombatRoutine.GetPropertyInt(InvokeNiuzao)];
+        private string UseStagger => StaggerList[CombatRoutine.GetPropertyInt(InvokeNiuzao)];
         private int PurifyingBrewStaggerPercentProc => CombatRoutine.GetPropertyInt("PurifyingBrewStaggerPercentProc");
 
 
@@ -52,8 +55,13 @@ namespace HyperElk.Core
         private string HealingElixir = "Healing Elixir";
         private string ChiBurst = "Chi Burst";
         private string RushingJadeWind = "Rushing Jade Wind";
-        private string InvokeNiuzao = "Invoke Niuzao, the Black Ox";
+        private string InvokeNiuzao = "Invoke Niuzao,  the Black Ox";
+        private string ExplodingKeg = "Exploding Keg";
+        private string Stagger = "Stagger";
 
+        private string LightStagger = "Light Stagger";
+        private string ModerateStagger = "Moderate Stagger";
+        private string HeavyStagger = "Heavy Stagger";
         public override void Initialize()
         {
             CombatRoutine.Name = "Brewmaster Monk @Mufflon12";
@@ -64,7 +72,10 @@ namespace HyperElk.Core
             CombatRoutine.AddProp(CelestialBrew, "Celestial Brew", numbList, "Life percent at which " + CelestialBrew + " is used, set to 0 to disable set 100 to use it everytime", "Healing", 5);
             CombatRoutine.AddProp(FortifyingBrew, "Fortifying Brew", numbList, "Life percent at which " + FortifyingBrew + " is used, set to 0 to disable set 100 to use it everytime", "Healing", 4);
             CombatRoutine.AddProp(HealingElixir, "Healing Elixir", numbList, "Life percent at which " + HealingElixir + " is used, set to 0 to disable set 100 to use it everytime", "Healing", 8);
-            CombatRoutine.AddProp("PurifyingBrewStaggerPercentProc", "PurifyingBrew", 4, "Use PurifyingBrew, compared to max life. On 1000 HP 20% means Cast if stagger is higher than 200", "Healing");
+            CombatRoutine.AddProp("PurifyingBrewStaggerPercentProc", "PurifyingBrew", 4, "Use PurifyingBrew, compared to max life. On 1000 HP 20% means Cast if stagger is higher than 200", "Stagger Management");
+            CombatRoutine.AddProp(InvokeNiuzao, "Use " + InvokeNiuzao, InvokeNiuzaoList, "Use " + InvokeNiuzao + "always, with Cooldowns, On AOE", "Cooldowns", 0);
+            CombatRoutine.AddProp(Stagger, "Use " + PurifyingBrew, StaggerList, "Use " + PurifyingBrew + " 2nd charge always, On Light / Moderate / Heavy Stagger", "Stagger Management", 2);
+
 
 
             //Spells
@@ -78,6 +89,7 @@ namespace HyperElk.Core
             CombatRoutine.AddSpell(InvokeNiuzao, "D8");
             CombatRoutine.AddSpell(ChiBurst, "D9");
             CombatRoutine.AddSpell(RushingJadeWind, "D0");
+            CombatRoutine.AddSpell(ExplodingKeg, "Oem6");
 
 
             CombatRoutine.AddSpell(Vivify, "NumPad1");
@@ -93,7 +105,9 @@ namespace HyperElk.Core
 
 
             //Debuffs
-
+            CombatRoutine.AddDebuff(LightStagger);
+            CombatRoutine.AddDebuff(ModerateStagger);
+            CombatRoutine.AddDebuff(HeavyStagger);
 
 
         }
@@ -125,6 +139,65 @@ namespace HyperElk.Core
                 return;
             }
 
+            //AOE
+            if(IsAOE && API.PlayerUnitInMeleeRangeCount >= AOEUnitNumber)
+            {
+                //InvokeNiuzao
+                if (!API.SpellISOnCooldown(InvokeNiuzao) && API.PlayerLevel >= 42 && (UseInvokeNiuzao == "always" || UseInvokeNiuzao == "with Cooldowns" && IsCooldowns))
+                {
+                    API.CastSpell(InvokeNiuzao);
+                    return;
+                }
+                //KegSmash
+                if (!API.SpellISOnCooldown(KegSmash) && API.PlayerEnergy > 40 && API.PlayerLevel >= 21)
+                {
+                    API.CastSpell(KegSmash);
+                    return;
+                }
+                //BlackOutKick
+                if (API.CanCast(BlackOutKick) && API.PlayerLevel >= 2)
+                {
+                    API.CastSpell(BlackOutKick);
+                    return;
+                }
+                //BlackOutKick
+                if (API.CanCast(BlackOutKick) && API.PlayerLevel >= 2)
+                {
+                    API.CastSpell(BlackOutKick);
+                    return;
+                }
+                //Breath of Fire
+                if (!API.SpellISOnCooldown(BreathOfFire) && API.PlayerLevel >= 29)
+                {
+                    API.CastSpell(BreathOfFire);
+                    return;
+                }
+                //ChiBurst
+                if (API.CanCast(ChiBurst) && API.PlayerIsTalentSelected(1, 3))
+                {
+                    API.CastSpell(ChiBurst);
+                    return;
+                }
+                //Rushing Jade Wind
+                if (API.CanCast(RushingJadeWind) && !API.SpellISOnCooldown(RushingJadeWind) && API.PlayerIsTalentSelected(6, 2))
+                {
+                    API.CastSpell(RushingJadeWind);
+                    return;
+                }
+                //Exploping Kek
+                if (API.CanCast(ExplodingKeg) && API.PlayerIsTalentSelected(6, 3))
+                {
+                    API.CastSpell(ExplodingKeg);
+                    return;
+                }
+                //Spinning Crane Kick
+                if (API.CanCast(SpinningCraneKick) && API.PlayerEnergy >= 40 && API.PlayerEnergy > 40 && API.PlayerLevel >= 7)
+                {
+                    API.CastSpell(SpinningCraneKick);
+                    return;
+                }
+            }
+
             //HEALING
             //Healing Elixir
             if (API.PlayerHealthPercent <= HealingElixirLifePercentProc && !API.SpellISOnCooldown(HealingElixir) && API.PlayerIsTalentSelected(5, 2))
@@ -145,7 +218,37 @@ namespace HyperElk.Core
                 return;
             }
             //Purifying Brew
-            if (!API.SpellISOnCooldown(PurifyingBrew) && API.PlayerStaggerPercent >= PurifyingBrewStaggerPercentProc && PlayerLevel >= 23)
+            if (!API.SpellISOnCooldown(PurifyingBrew) && API.PlayerStaggerPercent >= PurifyingBrewStaggerPercentProc && PlayerLevel >= 23 && PlayerLevel <=47)
+            {
+                API.CastSpell(PurifyingBrew);
+                return;
+            }
+            //Purifying Brew 2nd Charge
+            if (!API.SpellISOnCooldown(PurifyingBrew) && API.PlayerStaggerPercent >= PurifyingBrewStaggerPercentProc && PlayerLevel >= 47 && (UseStagger == "always"))
+            {
+                API.CastSpell(PurifyingBrew);
+                return;
+            }
+            //Purifying Brew 2nd Charge Light stagger
+            if (!API.SpellISOnCooldown(PurifyingBrew) && API.PlayerStaggerPercent >= PurifyingBrewStaggerPercentProc && PlayerLevel >= 47 && (UseStagger == "Light Stagger") && API.PlayerHasDebuff(LightStagger))
+            {
+                API.CastSpell(PurifyingBrew);
+                return;
+            }
+            //Purifying Brew 2nd Charge Moderate stagger
+            if (!API.SpellISOnCooldown(PurifyingBrew) && API.PlayerStaggerPercent >= PurifyingBrewStaggerPercentProc && PlayerLevel >= 47 && (UseStagger == "Moderate Stagger") && API.PlayerHasDebuff(ModerateStagger))
+            {
+                API.CastSpell(PurifyingBrew);
+                return;
+            }
+            //Purifying Brew 2nd Charge Heavy stagger
+            if (!API.SpellISOnCooldown(PurifyingBrew) && API.PlayerStaggerPercent >= PurifyingBrewStaggerPercentProc && PlayerLevel >= 47 && (UseStagger == "Heavy Stagger") && API.PlayerHasDebuff(HeavyStagger))
+            {
+                API.CastSpell(PurifyingBrew);
+                return;
+            }
+            //Purifying Brew
+            if (!API.SpellISOnCooldown(PurifyingBrew) && API.PlayerStaggerPercent >= PurifyingBrewStaggerPercentProc && PlayerLevel >= 47)
             {
                 API.CastSpell(PurifyingBrew);
                 return;
@@ -163,29 +266,17 @@ namespace HyperElk.Core
                 return;
             }
 
-            //ROTATION
+            //ROTATION  SINGLE TARGET
+            //InvokeNiuzao
+            if (!API.SpellISOnCooldown(InvokeNiuzao) && API.PlayerLevel >= 42 && (UseInvokeNiuzao == "always" || UseInvokeNiuzao == "with Cooldowns" && IsCooldowns))
+            {
+                API.CastSpell(InvokeNiuzao);
+                return;
+            }
             //Keg Smash
             if (!API.SpellISOnCooldown(KegSmash) && API.PlayerEnergy > 40 && API.PlayerLevel >= 21)
             {
                 API.CastSpell(KegSmash);
-                return;
-            }
-            //Breath of Fire
-            if (!API.SpellISOnCooldown(BreathOfFire) && API.PlayerLevel >= 29)
-            {
-                API.CastSpell(BreathOfFire);
-                return;
-            }
-            //Rushing Jade Wind
-            if (IsAOE && API.CanCast(RushingJadeWind) && !API.SpellISOnCooldown(RushingJadeWind) && API.PlayerUnitInMeleeRangeCount >= AOEUnitNumber && API.PlayerIsTalentSelected(6, 2))
-            {
-                API.CastSpell(RushingJadeWind);
-                return;
-            }
-            //Spinning Crane Kick
-            if (IsAOE && API.CanCast(SpinningCraneKick) && API.PlayerEnergy >= 40 && API.PlayerUnitInMeleeRangeCount >= AOEUnitNumber && API.PlayerEnergy > 40 && API.PlayerLevel >= 7)
-            {
-                API.CastSpell(SpinningCraneKick);
                 return;
             }
             //Blackout Kick
@@ -194,13 +285,19 @@ namespace HyperElk.Core
                 API.CastSpell(BlackOutKick);
                 return;
             }
+            //Breath of Fire
+            if (!API.SpellISOnCooldown(BreathOfFire) && API.PlayerLevel >= 29)
+            {
+                API.CastSpell(BreathOfFire);
+                return;
+            }
             //ChiBurst
             if (API.CanCast(ChiBurst) && API.PlayerIsTalentSelected(1, 3))
             {
                 API.CastSpell(ChiBurst);
                 return;
             }
-            //Tiger Palm
+            //Tiger Palm -> nothing else to do 
             if (API.CanCast(TigerPalm) && API.PlayerEnergy >= 50)
             {
                 API.CastSpell(TigerPalm);
