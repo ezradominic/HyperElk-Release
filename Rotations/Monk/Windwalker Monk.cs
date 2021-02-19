d06f89fa398771e64648fe92e3abf4768eff4ed1
mufflon11 <mufflon12@googlemail.com>
2021-02-19 14:05:19 +0100

    Windwalker Tweaks

--- Rotations/Monk/Windwalker Monk.cs
+++ Rotations/Monk/Windwalker Monk.cs
@@ -42,7 +42,7 @@ namespace HyperElk.Core
         private int TouchofKarmaPercentProc => numbList[CombatRoutine.GetPropertyInt(TouchofKarma)];
 
         int[] numbList = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100 };
-        private bool NotCasting => API.PlayerCurrentCastTimeRemaining <= 0;
+        private bool NotCasting => !API.PlayerIsCasting(false);
         bool LastCastTigerPalm => API.PlayerLastSpell == TigerPalm;
         bool LastCastBlackoutkick => API.LastSpellCastInGame == BlackOutKick;
         bool LastCastSpinningCraneKick => API.LastSpellCastInGame == SpinningCraneKick;
@@ -401,7 +401,7 @@ namespace HyperElk.Core
                     return;
                 }
                 //actions.aoe+=/chi_burst,if=chi.max-chi>=2
-                if (API.CanCast(ChiBurst) && TalentChiBurst && ChiDeficit >= 2 && !API.PlayerIsMoving && !API.PlayerIsCasting(true))
+                if (API.CanCast(ChiBurst) && TalentChiBurst && ChiDeficit >= 2 && !API.PlayerIsMoving)
                 {
                     API.CastSpell(ChiBurst);
                     return;
@@ -764,7 +764,7 @@ namespace HyperElk.Core
                     return;
                 }
                 //actions.cd_serenity+=/touch_of_death,if=fight_remains>180|pet.xuen_the_white_tiger.active|fight_remains<10
-                if (IsCooldowns && !API.SpellISOnCooldown(TouchofDeath) && API.TargetHealthPercent >= 0 && API.TargetMaxHealth < API.PlayerMaxHealth && (UseTouchofDeath == "with Cooldowns"))
+                if (IsCooldowns && !API.SpellISOnCooldown(TouchofDeath) && API.TargetHealthPercent <= 15 && (UseTouchofDeath == "with Cooldowns"))
                 {
                     API.CastSpell(TouchofDeath);
                     return;
@@ -826,7 +826,7 @@ namespace HyperElk.Core
                     return;
                 }
                 //actions.cd_sef+=/touch_of_death,if=buff.storm_earth_and_fire.down&pet.xuen_the_white_tiger.active|fight_remains<10|fight_remains>180
-                if (IsCooldowns && !API.SpellISOnCooldown(TouchofDeath) && API.TargetHealthPercent >= 0 && API.TargetMaxHealth < API.PlayerMaxHealth && (UseTouchofDeath == "with Cooldowns"))
+                if (IsCooldowns && !API.SpellISOnCooldown(TouchofDeath) && API.TargetHealthPercent <= 15 && (UseTouchofDeath == "with Cooldowns"))
                 {
                     API.CastSpell(TouchofDeath);
                     return;
